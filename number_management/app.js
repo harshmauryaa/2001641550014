7ffa7698cea969531a31336a1f4f2d1c6c6cc4d1
Harsh <harshmaurya88969@gmail.com>
2023-10-06 11:37:56 +0530

    Add fetchNumbers function for fetching data from URLs

--- number_management/app.js
+++ number_management/app.js
@@ -2,3 +2,36 @@ const http = require('http');
 const url = require('url');
 const {promisify} = require('util');
 const dnsLookup = promisify(require('dns').lookup);
+
+const TIMEOUT = 500;
+
+async function fetchNumbers(targetUrl) {
+    return new Promise((resolve, reject) => {
+        let req = http.get(targetUrl, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    let parsedData = JSON.parse(data);
+                    resolve(parsedData.numbers);
+                } catch (e) {
+                    resolve([]);
+                }
+            });
+        });
+
+        req.on('error', (err) => {
+            resolve([]);
+        });
+
+        req.setTimeout(TIMEOUT, () => {
+            req.destroy();
+            resolve([]);
+        });
+    });
+}
+
